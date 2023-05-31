c9608e185bab11bb6ca3c9c587b69eed408d8b79
SAsimRaza <asimraza2016@gmail.com>
2023-05-31 19:59:54 +0500

    feat: update test cases

--- test/DeXaPresale.t.sol
+++ test/DeXaPresale.t.sol
@@ -61,71 +61,12 @@ contract TokenTest is Test {
         vm.stopPrank();
     }
 
-    function testDeployments() public {
+    function testDeployments() public view {
         assert(address(deXa) != address(0));
         assert(address(deXaPresale) != address(0));
     }
 
     function testUserAllowance() public {
-        vm.startPrank(owner);
-        deXaPresale.allowanceToUser(user1, 100e18);
-
-        assertEq(
-            deXaPresale.claimableTokens(address(user1)),
-            100e18,
-            "Amount mismatched"
-        );
-    }
-
-    function testUsersAllowance() public {
-        vm.startPrank(owner);
-
-        address[] memory _users = new address[](2);
-        _users[0] = address(user1);
-        _users[1] = address(user2);
-
-        uint256[] memory _allowances = new uint256[](2);
-        _allowances[0] = uint256(100e18);
-        _allowances[1] = uint256(10e18);
-
-        deXaPresale.batchAllowanceToUsers(_users, _allowances);
-
-        uint256 _amount0 = deXaPresale.claimableTokens(_users[0]);
-        uint256 _amount1 = deXaPresale.claimableTokens(_users[1]);
-
-        assertEq(_amount0, _allowances[0], "Amount mismatched");
-        assertEq(_amount1, _allowances[1], "Amount mismatched");
-    }
-
-    function testClaimTokens() public {
-        vm.startPrank(owner);
-        deal({token: address(deXa), to: address(deXaPresale), give: 10000e18});
-
-        address[] memory _users = new address[](2);
-        _users[0] = address(user1);
-        _users[1] = address(user2);
-
-        uint256[] memory _allowances = new uint256[](2);
-        _allowances[0] = uint256(100e18);
-        _allowances[1] = uint256(10e18);
-
-        deXaPresale.batchAllowanceToUsers(_users, _allowances);
-
-        uint256 _amount0 = deXaPresale.claimableTokens(_users[0]);
-        uint256 _amount1 = deXaPresale.claimableTokens(_users[1]);
-        vm.stopPrank();
-
-        vm.startPrank(user1);
-        deXaPresale.claimPrebookTokens();
-
-        assertEq(
-            deXa.balanceOf(user1),
-            100e18,
-            "Not received claimable amount"
-        );
-    }
-
-    function testSetRound0AndPurchaseWithMinBUSD() public {
         vm.startPrank(owner);
         deal({
             token: address(deXa),
@@ -143,6 +84,73 @@ contract TokenTest is Test {
             1000e18
         );
 
+        deXaPresale.allowanceToUser(user1, 150e18, 0);
+
+        assertEq(
+            deXaPresale.claimableTokens(address(user1)),
+            150e18,
+            "Amount mismatched"
+        );
+    }
+
+    function testUsersAllowance() public {
+        vm.startPrank(owner);
+
+        deal({
+            token: address(deXa),
+            to: address(deXaPresale),
+            give: 50000000e18
+        });
+        deXaPresale.setRoundInfoForBusd(
+            0,
+            800000000000000000,
+            block.timestamp,
+            block.timestamp + 30 days,
+            1,
+            50000000e18,
+            5e18,
+            1000e18
+        );
+
+        address[] memory _users = new address[](2);
+        _users[0] = address(user1);
+        _users[1] = address(user2);
+
+        uint256[] memory _allowances = new uint256[](2);
+        _allowances[0] = uint256(100e18);
+        _allowances[1] = uint256(10e18);
+
+        uint256[] memory _rounds = new uint256[](2);
+        _rounds[0] = 0;
+        _rounds[1] = 0;
+
+        deXaPresale.batchAllowanceToUsers(_users, _allowances, _rounds);
+
+        uint256 _amount0 = deXaPresale.claimableTokens(_users[0]);
+        uint256 _amount1 = deXaPresale.claimableTokens(_users[1]);
+
+        assertEq(_amount0, _allowances[0], "Amount mismatched");
+        assertEq(_amount1, _allowances[1], "Amount mismatched");
+    }
+
+    function testSetRound0AndPurchaseWithMinBUSD() public {
+        vm.startPrank(owner);
+        deal({
+            token: address(deXa),
+            to: address(deXaPresale),
+            give: 50000000e18
+        });
+        deXaPresale.setRoundInfoForBusd(
+            0,
+            800000000000000000,
+            block.timestamp,
+            block.timestamp + 30 days,
+            4,
+            50000000e18,
+            150e18,
+            1000e18
+        );
+
         vm.stopPrank();
         vm.startPrank(user1);
 
@@ -150,25 +158,28 @@ contract TokenTest is Test {
 
         deXaPresale.tokenPurchaseWithBUSD(150e18);
 
-        vm.warp(31 days);
+        vm.warp(block.timestamp + 30 days * 12);
+        console2.log(block.timestamp + 30 days * 12);
         deXaPresale.claimTokensFromBusd(0);
 
         assertEq(
             deXa.balanceOf(address(user1)),
-            18750000000000000000,
+            187500000000000000000,
             "Amount not received as expected"
         );
     }
 
     function testSetRound0AndPurchaseWithMaxBUSD() public {
         vm.startPrank(owner);
+
         deal({token: address(deXa), to: address(deXaPresale), give: 500000e18});
+
         deXaPresale.setRoundInfoForBusd(
             0,
             800000000000000000,
             block.timestamp,
             block.timestamp + 30 days,
-            1,
+            4,
             1000e18,
             150e18,
             5000e18
@@ -181,12 +192,12 @@ contract TokenTest is Test {
 
         deXaPresale.tokenPurchaseWithBUSD(4500e18);
 
-        vm.warp(31 days);
+        vm.warp(block.timestamp + 30 days * 12);
         deXaPresale.claimTokensFromBusd(0);
 
         assertEq(
             deXa.balanceOf(address(user1)),
-            562500000000000000000,
+            5625000000000000000000,
             "Amount not received as expected"
         );
     }
@@ -204,12 +215,12 @@ contract TokenTest is Test {
             800000000000000000,
             block.timestamp,
             block.timestamp + 30 days,
-            1,
+            4,
             1000e18,
             150e18,
             5000e18
         );
-        vm.warp(31 days);
+        vm.warp(block.timestamp + 31 days * 4);
 
         //round2
         deXaPresale.setRoundInfoForBusd(
@@ -217,7 +228,7 @@ contract TokenTest is Test {
             800000000000000000,
             block.timestamp,
             block.timestamp + 30 days,
-            1,
+            4,
             50000000e18,
             150e18,
             5000e18
@@ -230,14 +241,63 @@ contract TokenTest is Test {
 
         deXaPresale.tokenPurchaseWithBUSD(4500e18);
 
-        vm.warp(33 days);
+        vm.warp(block.timestamp + 30 days * 12);
 
         deXaPresale.claimTokensFromBusd(1);
 
         assertEq(
             deXa.balanceOf(address(user1)),
-            562500000000000000000,
+            5625000000000000000000,
             "Amount not received as expected"
         );
+    }
+
+    function testClaimTokens() public {
+        vm.startPrank(owner);
+        deal({
+            token: address(deXa),
+            to: address(deXaPresale),
+            give: 50000000e18
+        });
+        deXaPresale.setRoundInfoForBusd(
+            0,
+            800000000000000000,
+            block.timestamp,
+            block.timestamp + 30 days,
+            1,
+            50000000e18,
+            5e18,
+            1000e18
+        );
+
+        address[] memory _users = new address[](2);
+        _users[0] = address(user1);
+        _users[1] = address(user2);
+
+        uint256[] memory _allowances = new uint256[](2);
+        _allowances[0] = uint256(100e18);
+        _allowances[1] = uint256(10e18);
+
+        uint256[] memory _rounds = new uint256[](2);
+        _rounds[0] = 0;
+        _rounds[1] = 0;
+
+        deXaPresale.batchAllowanceToUsers(_users, _allowances, _rounds);
+
+        // uint256 _amount0 = deXaPresale.claimableTokens(_users[0]);
+        // uint256 _amount1 = deXaPresale.claimableTokens(_users[1]);
+        vm.stopPrank();
+
+        vm.startPrank(user1);
+        // deXaPresale.claimPrebookTokens();
+
+        vm.warp(block.timestamp + 30 days * 12);
+        deXaPresale.claimTokensFromBusd(0);
+
+        // assertEq(
+        //     deXa.balanceOf(user1),
+        //     100e18,
+        //     "Not received claimable amount"
+        // );
     }
 }
