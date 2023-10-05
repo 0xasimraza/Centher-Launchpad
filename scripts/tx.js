999aebf20c285d591813dbcc32ca3b52a281bc47
SAsimRaza <asimraza2016@gmail.com>
2023-10-05 12:06:28 +0500

    feat: update scripts

--- scripts/tx.js
+++ scripts/tx.js
@@ -9,73 +9,75 @@ const launchpad = "0x5Eaf2D08FA62220AC064Df5e47521cB7cc16F964"; //testnet
 let provider;
 
 async function main() {
-    //   await createAllowanceForBusdUsers();
-    await createAllowanceForNtrUsers();
+  await createAllowanceForBusdUsers();
+  //   await createAllowanceForNtrUsers();
 }
 
 function getContract(address, abi) {
-    return new ethers.Contract(address, abi, getSigner());
+  return new ethers.Contract(address, abi, getSigner());
 }
 
 function getPrivateKey() {
-    return process.env.PRIVATE_KEY;
+  return process.env.PRIVATE_KEY;
 }
 
 function getSigner() {
-    return new ethers.Wallet(getPrivateKey(), getProvider());
+  return new ethers.Wallet(getPrivateKey(), getProvider());
 }
 
 function getProvider() {
-    if (process.env.NETWORK === "goerli") {
-        provider = new ethers.JsonRpcProvider(
-            "https://goerli.infura.io/v3/b17715f3b04d4ccb90389a946de9c598"
-        );
+  if (process.env.NETWORK === "goerli") {
+    provider = new ethers.JsonRpcProvider(
+      "https://goerli.infura.io/v3/b17715f3b04d4ccb90389a946de9c598"
+    );
 
-        return provider;
-    } else if (process.env.NETWORK === "bsc") {
-        return new ethers.JsonRpcProvider(process.env.BSC_URL);
-    } else {
-        return "Not Valid Network";
-    }
+    return provider;
+  } else if (process.env.NETWORK === "bsc") {
+    return new ethers.JsonRpcProvider(process.env.BSC_URL);
+  } else {
+    return "Not Valid Network";
+  }
 }
 
 async function createAllowanceForBusdUsers() {
-    console.log("Current Signer: ", await getSigner());
+  console.log("Current Signer: ", await getSigner());
 
-    const contract = getContract(launchpad, launchpadAbi);
+  const contract = getContract(launchpad, launchpadAbi);
 
-    try {
-        let tx = await contract.batchAllowanceToBusdUsers(
-            ["0xF4988Cad71BDd524bb613c499dcb0e296617253f"],
-            [parseEther("1500")],
-            [0]
-        );
-        await tx.wait();
-        console.log("hash: ", await tx.hash);
-    } catch (error) {
-        console.log("error: ", error);
-    }
+  try {
+    let tx = await contract.batchAllowanceToBusdUsers(
+      ["0x71b8946E56dB409AD4496003F9fA2b0624c28C9F"],
+      [parseEther("83332.42296667122")],
+      [1],
+      "1696251600"
+    );
+    await tx.wait();
+    console.log("hash: ", await tx.hash);
+  } catch (error) {
+    console.log("error: ", error);
+  }
 }
 
 async function createAllowanceForNtrUsers() {
-    console.log("Current Signer: ", await getSigner());
+  console.log("Current Signer: ", await getSigner());
 
-    const contract = getContract(launchpad, launchpadAbi);
+  const contract = getContract(launchpad, launchpadAbi);
 
-    try {
-        let tx = await contract.batchAllowanceToNtrUsers(
-            ["0xF4988Cad71BDd524bb613c499dcb0e296617253f"],
-            [parseEther("1000")],
-            [0]
-        );
-        await tx.wait();
-        console.log("hash: ", await tx.hash);
-    } catch (error) {
-        console.log("error: ", error);
-    }
+  try {
+    let tx = await contract.batchAllowanceToNtrUsers(
+      ["0x71b8946E56dB409AD4496003F9fA2b0624c28C9F"],
+      [parseEther("1000")],
+      [0],
+      "1695638001"
+    );
+    await tx.wait();
+    console.log("hash: ", await tx.hash);
+  } catch (error) {
+    console.log("error: ", error);
+  }
 }
 
 main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
+  console.error(error);
+  process.exitCode = 1;
 });
